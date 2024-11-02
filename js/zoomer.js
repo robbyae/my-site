06eebce3e1c572e2eba2c77aae20bfebc6d29b40
robbyae <robbyastrong@gmail.com>
2024-11-02 17:10:40 -0500

    mobile detect

--- js/zoomer.js
+++ js/zoomer.js
@@ -1,6 +1,12 @@
 $(document).ready(function(){
-    if($(window).width() >= '800') {
+    const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+    if (!isMobile) {
         $('.zoom-span')
         .zoom();
+        $('#hover-zoom').css('display','none');
     }
+    // if($(window).width() >= '800') {
+    //     $('.zoom-span')
+    //     .zoom();
+    // }
   });
